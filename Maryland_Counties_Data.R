0c1af0eaa118b61714ef230b0b73e2bb74270de1
mathew <mathewchan28@gmail.com>
2022-04-25 13:40:38 -0400

    Final changes to organizing the code.

--- Maryland_Counties_Data.R
+++ Maryland_Counties_Data.R
@@ -1,7 +1,7 @@
 #Script made by Mathew Chan, supervised by Richard Latham
 
 
-# Retrieving data -----------------------------------------------------
+# Retrieving Data -----------------------------------------------------
 
 
 #Loads in installed packages
@@ -18,7 +18,7 @@ Sys.getenv("CENSUS_API_KEY")
 #Loads census data that was retrieved from API key
 var = load_variables(2019,"acs5",cache=TRUE) 
 
-#Filtering out data based on census concepts
+#Filters out data based on census concepts
 vars = var %>% dplyr::filter(grepl("B08006", name, label)) %>% 
   rbind(var %>% dplyr::filter(grepl("B08203", name, label))) %>%
   rbind(var %>% dplyr::filter(grepl("B08141", name, label))) %>%
@@ -48,19 +48,19 @@ df3 = get_acs(geography="county", state = "MD",variables = vars %>% select(name)
 df4 = get_acs(geography="county", state = "MD",variables = vars %>% select(name) %>% unlist() %>% unname(),year=2016,keep_geo_vars=TRUE, survey = "acs1") %>% mutate(year = 2016)
 
 
-# Manipulating data -------------------------------------------------------
+# Manipulating Data -------------------------------------------------------
 
 
-#row binds all years of data from 2016 to 2019
+#Row binds all years of data from 2016 to 2019
 all_df = df %>% 
   rbind(df2) %>%
   rbind(df3) %>%
   rbind(df4) 
 
-#reorganizing data from row-rise to column-wise 
+#Reorganizes data from row-rise to column-wise 
 all_county_df = all_df %>% select(-moe) %>% pivot_wider(names_from = "variable", values_from = "estimate")
 
-#Turning absolute numbers into proportions by dividing total population of the group by the total population of the county
+#Turns absolute numbers into proportions by dividing total population of the group by the total population of the county
 multi_variable_data = all_county_df %>% select(GEOID, NAME, year, B01001_001, B25044_009, B08203_007, B25034_011, B28011_002, B02001_002, B14007_017, B18103_001, B08302_005, B08303_008, B25024_003, B08203_003 ,B08203_004 ,B08203_005 ,B08203_006, B19001_012, B08203_013) %>% 
   mutate(Renter_housing_prop = B25044_009/B01001_001) %>%
   mutate(No_home_workers_prop = B08203_007/B01001_001) %>%
@@ -76,49 +76,53 @@ multi_variable_data = all_county_df %>% select(GEOID, NAME, year, B01001_001, B2
   mutate(One_worker_at_home_prop = B08203_013/B01001_001) %>%
   mutate(vehicle_ownership_prop = (B08203_003 + B08203_004 + B08203_005 + B08203_006)/ B01001_001)
 
-#plotting graph vs. graph (scatterplot matrix) of all comparison graphs
-#pairs(multi_variable_data %>% select(-c(NAME, GEOID)) %>% select(c(Depart_work_6AM_629AM_prop , Travel_time_work_30_to_34_mins_prop , Attached_1_unit_building_prop , Household_income_60k_to_70k_prop , Lives_outside_principal_city_prop , Public_transit_work_prop , Renter_housing_prop , Structure_built_1939_earlier_prop , One_worker_at_home_prop)))
+#Plots graph vs. graph (scatter plot matrix) of all comparison graphs
+pairs(multi_variable_data %>% select(-c(NAME, GEOID)) %>% select(c(College_undergraduate_prop, Travel_time_work_30_to_34_mins_prop, Structure_built_1939_earlier_prop, Vision_difficulty_prop, Lives_outside_principal_city_prop, White_race_prop, Internet_subscription_prop, Attached_1_unit_building_prop)))
 
-#training data
+#Trains data
 df_train =  multi_variable_data %>% subset(year<2019) %>% select(-year)
 df_test = multi_variable_data %>% subset(year>=2019) %>% select(-year)
 
-#this line will say what variables are to be removed because of NA
-model = lm(data=df_train, vehicle_ownership_prop ~ Household_income_60k_to_70k_prop +  College_undergraduate_prop + Travel_time_work_30_to_34_mins_prop + Structure_built_1939_earlier_prop + Vision_difficulty_prop + White_race_prop + Internet_subscription_prop + Attached_1_unit_building_prop)
+#Creates the linear model
+model = lm(data=df_train, vehicle_ownership_prop ~ College_undergraduate_prop + Travel_time_work_30_to_34_mins_prop + Structure_built_1939_earlier_prop + Vision_difficulty_prop + White_race_prop + Internet_subscription_prop + Attached_1_unit_building_prop)
 
 
-# Summarizing data --------------------------------------------------------
+# Summarizing Data --------------------------------------------------------
 
 
-#summary of the model
+#Summary of the model
 summary(model)
 
-#tells total number of data that is not available in the data set (NA)
+#Tells total number of data that is not available in the data set (NA)
 is.na(df_test) %>% sum()
 
-#uses backwards selection in step-wise function to finds fit of variables into model
+#Uses backwards selection/elimination in step-wise function to finds fit of variables into model
 step(model)
 
-#residual plots of model
+#Residual plots of model
 plot(model)
 
-#variable for prediction
+#Variable for prediction
 predict(model,newdata = df_test)
 
-#predicting the years from 2019 onward
+#Predicts the years from 2019 onward
 df_test$pred = predict(model,newdata = df_test)
 df_test %>% select(pred, vehicle_ownership_prop, everything())
 
-#graph/plot predictions
+#Tests correlation of individual variables in the model against vehicle ownership. x can equal any variable from the census
+df_test %>%
+  ggplot(aes(x=Internet_subscription_prop,y=vehicle_ownership_prop)) + geom_point() + geom_line() + scale_x_continuous(name = "Variable") + scale_y_continuous(name = "Vehicle Ownership")
+
+#Graphs/plots predictions
 df_test %>%
   ggplot() + theme_bw() +
-  geom_point(aes(x=pred,y=vehicle_ownership_prop)) + # plotting predicted vs actual
-  geom_line(aes(x=vehicle_ownership_prop,y=vehicle_ownership_prop),color="blue",linetype=2) + # plotting actual data
+  geom_point(aes(x=pred,y=vehicle_ownership_prop)) + # Plots predicted vs actual
+  geom_line(aes(x=vehicle_ownership_prop,y=vehicle_ownership_prop),color="blue",linetype=2) + 
+  # Plots actual data
   #ggrepel::geom_label_repel(aes(x=vehicle_ownership_prop,y=vehicle_ownership_prop,label = NAME)) +
   #geom_smooth(aes(x=pred,y=vehicle_ownership_prop),color="black") + # plotting predicted vs actual
   #scale_x_continuous(limits=c(4,6),name = "Predicted") +
   #scale_y_continuous(limits=c(4,6),name = "Actual") +
-  ggtitle("Model Performance - Predicted vs. Actual") + # title of plot
+  ggtitle("Model Performance - Predicted vs. Actual") + # Title of plot
   xlab("Prediction") + # x axis title
   ylab("Proportion of Household Vehicle Ownership") # y axis title
-
--- Pennsylvania_and_Maryland_Counties_Data.R
+++ Pennsylvania_and_Maryland_Counties_Data.R
@@ -1,164 +1,120 @@
-#This code was used for an exprimental purpose.
+#Script made by Mathew Chan, supervised by Richard Latham
+#This code was used for an experimental purpose.
 
+
+# Retrieving Data ---------------------------------------------------------
+
+
+#Loads in installed packages
 library(tidyverse)
 library(tidycensus)
 library(plyr)
 library(ggplot2)
+library(ggrepel)
 library(patchwork)
 
 #Retrieves census data through use of requested API key
 Sys.getenv("CENSUS_API_KEY") 
 
-#loads census data that was retrieved from API key
-var = load_variables(2019,"acs5",cache=TRUE) 
+#Loads census data that was retrieved from API key
+var_combo = load_variables(2019,"acs5",cache=TRUE) 
 
-#B08203_007 _ no workers in house
+#Filters out data based on census concepts
+vars_combo = var_combo %>% dplyr::filter(grepl("B08006", name, label)) %>% 
+  rbind(var_combo %>% dplyr::filter(grepl("B08203", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B08141", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B25044", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B05011", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B08016", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B28006", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B25034", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B25121", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B28011", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B16001", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B23007", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B02001", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B14007", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B18103", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B08302", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B08303", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B16001", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B25024", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B19001", name, label))) %>%
+  rbind(var_combo %>% dplyr::filter(grepl("B01001", name, label)))  
 
-#B25044_002 _ owner occupied housing
+#Retrieves variables based on 1 year of American Community Survey data from 2016 to 2019
+df_combo = get_acs(geography="county", state = c("PA","MD"),variables = vars_combo %>% select(name) %>% unlist() %>% unname(),year=2019,keep_geo_vars=TRUE, survey = "acs1") %>% mutate(year = 2019)
+df2_combo = get_acs(geography="county", state = c("PA","MD"),variables = vars_combo %>% select(name) %>% unlist() %>% unname(),year=2018,keep_geo_vars=TRUE, survey = "acs1") %>% mutate(year = 2018)
+df3_combo = get_acs(geography="county", state = c("PA","MD"),variables = vars_combo %>% select(name) %>% unlist() %>% unname(),year=2017,keep_geo_vars=TRUE, survey = "acs1") %>% mutate(year = 2017)
+df4_combo = get_acs(geography="county", state = c("PA","MD"),variables = vars_combo %>% select(name) %>% unlist() %>% unname(),year=2016,keep_geo_vars=TRUE, survey = "acs1") %>% mutate(year = 2016)
 
-#B08006_033 _ Other transit means 
 
-#	B08006_014 _ biked
+# Manipulating Data -------------------------------------------------------
 
-#	B08006_015 _ walked
 
-#B08006_008 _ public transportation
+#Row binds all years of data from 2016 to 2019
+all_df_combo = df_combo %>% 
+  rbind(df2_combo) %>%
+  rbind(df3_combo) %>%
+  rbind(df4_combo) 
 
-#B08203_007 _ workers in household
+#Reorganizes data from row-rise to column-wise
+all_county_df_combo = all_df_combo %>% select(-moe) %>% pivot_wider(names_from = "variable", values_from = "estimate")
 
-#B08141_031 _ worked from home 
+#Turns absolute numbers into proportions by dividing total population of the group by the total population of the county
+multi_variable_data_combo = all_county_df_combo %>% select(GEOID, NAME, year, B01001_001, B25044_009, B08203_007, B25034_011, B28011_002, B02001_002, B14007_017, B18103_001, B08302_005, B08303_008, B25024_003, B08203_003 ,B08203_004 ,B08203_005 ,B08203_006, B19001_012, B08203_013) %>% 
+  mutate(Renter_housing_prop_combo = B25044_009/B01001_001) %>%
+  mutate(No_home_workers_prop_combo = B08203_007/B01001_001) %>%
+  mutate(Structure_built_1939_earlier_prop_combo = B25034_011/B01001_001) %>%
+  mutate(Household_income_60k_to_70k_prop_combo = B19001_012/B01001_001) %>%
+  mutate(Internet_subscription_prop_combo = B28011_002/B01001_001) %>%
+  mutate(White_race_prop_combo = B02001_002/B01001_001) %>%
+  mutate(College_undergraduate_prop_combo = B14007_017/B01001_001) %>%
+  mutate(Vision_difficulty_prop_combo = B18103_001/B01001_001) %>%
+  mutate(Depart_work_6AM_629AM_prop_combo = B08302_005/B01001_001) %>%
+  mutate(Travel_time_work_30_to_34_mins_prop_combo = B08303_008/B01001_001) %>%
+  mutate(Attached_1_unit_building_prop_combo = B25024_003/B01001_001) %>%
+  mutate(One_worker_at_home_prop_combo = B08203_013/B01001_001) %>%
+  mutate(vehicle_ownership_prop_combo = (B08203_003 + B08203_004 + B08203_005 + B08203_006)/ B01001_001)
 
-#B25044_009 _ renter occupied <- not good indicator
+#Plots graph vs. graph (scatter plot matrix) of all comparison graphs
+pairs(multi_variable_data_combo %>% select(-c(NAME, GEOID)) %>% select(c(College_undergraduate_prop_combo, Travel_time_work_30_to_34_mins_prop_combo, Structure_built_1939_earlier_prop_combo, Vision_difficulty_prop_combo, White_race_prop_combo, Internet_subscription_prop_combo, Attached_1_unit_building_prop_combo)))
 
-#B25034_011 _ Structure built 1939 or earlier
 
-#	B08006_002 _ car, truck, van
-#B08303 = TRAVEL TIME TO WORK
-#B08302 = TIME OF DEPARTURE TO GO TO WORK
-#B08016 = PLACE OF WORK FOR WORKERS 16 YEARS AND OVER--METROPOLITAN STATISTICAL AREA LEVEL
-#B08017 = PLACE OF WORK FOR WORKERS 16 YEARS AND OVER--MICROPOLITAN STATISTICAL AREA LEVEL
-#B04007 = ANCESTRY
-#B28006 = EDUCATIONAL ATTAINMENT BY PRESENCE OF A COMPUTER AND TYPES OF INTERNET SUBSCRIPTION IN HOUSEHOLD
-#B25034 = YEAR STRUCTURE BUILT
-
-vars = var %>% dplyr::filter(grepl("B08006", name, label)) %>% 
-  rbind(var %>% dplyr::filter(grepl("B08203", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B08141", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B25044", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B05011", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B08016", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B28006", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B25034", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("C27021", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B25121", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B28011", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B16001", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B23007", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B02001", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B14007", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B18103", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B08302", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B08303", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B16001", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B25024", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B19001", name, label))) %>%
-  rbind(var %>% dplyr::filter(grepl("B01001", name, label))) 
-
-df = get_acs(geography="county", state = c("PA","MD"),variables = vars %>% select(name) %>% unlist() %>% unname(),year= 2017,keep_geo_vars=TRUE)
-
-all_county_df = df %>% select(-moe) %>% pivot_wider(names_from = "variable", values_from = "estimate")
-
-all_county_df %>% dplyr::rename("Renter_occupied_housing"="B25044_009")
-
-all_county_df %>% dplyr::rename("Owner_occupied_housing"="B25044_002")
-
-all_county_df %>% dplyr::rename("Public transit to work"="B08006_008")
-
-all_county_df %>% dplyr::rename("no workers in household"="B08203_007")
-
-all_county_df %>% dplyr::rename("Naturalized citizens"="B08016_002")
-
-all_county_df %>% dplyr::rename("Bachelor degree earned in computer present environment" = "B28006_014")
-
-all_county_df %>% dplyr::rename("1939 earlier strucutre built" = "B25034_011")
-
-all_county_df %>% dplyr::rename("Household income more than 100k, net worth more than 500k" = "B2 5121_106")
-
-all_county_df %>% dplyr::rename("Internet Subscription" = "B28011_002")
-
-all_county_df %>% dplyr::rename("Health Insurance Coverage" = "C27021_001")
-
-all_county_df %>% dplyr::rename("English only speakers" = "B16001_002")
-
-all_county_df %>% dplyr::rename("Families with children under 18" = "B23007_002")
-
-all_county_df %>% dplyr::rename("White people" = "B02001_002")
-
-all_county_df %>% dplyr::rename("College undergraduate" = "B14007_017")
-
-all_county_df %>% dplyr::rename("Vision difficulty" = "B18103_001")
-
-all_county_df %>% dplyr::rename("Depart to work 6:00AM to 6:29AM" = "B08302_005")
-
-all_county_df %>% dplyr::rename("Travel time 30 to 34 min to work" = "B08303_008")
-
-all_county_df %>% dplyr::rename("Attached 1 unit building" = "B25024_002")
-
-all_county_df %>% dplyr::rename("Live outside principal city" = "B08016_013")
-
-all_county_df %>% dplyr::rename("Total Population" = "B01001_001")
-
-multi_variable_data = all_county_df %>% select(GEOID, NAME, B01001_001, B25044_009, B08006_008, B08203_007, B25034_011, B25121_106, B28011_002, C27021_001, B16001_003, B23007_002, B02001_002, B14007_017, B18103_001, B08302_005, B08303_008, B25024_003, B08203_003 ,B08203_004 ,B08203_005 ,B08203_006, B19001_012, B08016_013, B08203_013) %>% 
-  mutate(Renter_housing_prop = B25044_009/B01001_001) %>%
-  mutate(Public_transit_work_prop = B08006_008/B01001_001) %>%
-  mutate(No_home_workers_prop = B08203_007/B01001_001) %>%
-  mutate(Structure_built_1939_earlier_prop = B25034_011/B01001_001) %>%
-  mutate(Household_income_60k_to_70k_prop = B19001_012/B01001_001) %>%
-  mutate(Internet_subscription_prop = B28011_002/B01001_001) %>%
-  mutate(Health_insurance_coverage_prop = C27021_001/B01001_001) %>%
-  mutate(Spanish_speakers_prop = B16001_003/B01001_001) %>%
-  mutate(Families_children_under_eighteen_prop = B23007_002/B01001_001) %>%
-  mutate(White_race_prop = B02001_002/B01001_001) %>%
-  mutate(College_undergraduate_prop = B14007_017/B01001_001) %>%
-  mutate(Vision_difficulty_prop = B18103_001/B01001_001) %>%
-  mutate(Depart_work_6AM_629AM_prop = B08302_005/B01001_001) %>%
-  mutate(Travel_time_work_30_to_34_mins_prop = B08303_008/B01001_001) %>%
-  mutate(Attached_1_unit_building_prop = B25024_003/B01001_001) %>%
-  mutate(Lives_outside_principal_city_prop = B08016_013/B01001_001) %>%
-  mutate(One_worker_at_home_prop = B08203_013/B01001_001) %>%
-  mutate(vehicle_ownership_prop = (B08203_003 + B08203_004 + B08203_005 + B08203_006)/ B01001_001)
-
-#multi_variable_data %>% ggplot() + geom_point(aes(x = Public_transit_work_prop , y= Structure_built_1939_earlier_prop)) #+
-#ggrepel::geom_label_repel(aes(x = Public_transit_work_prop  , y=No_home_workers_prop , label = NAME))
-
-#plotting graph vs. graph (scatterplot matrix) of all comparison graphs
-pairs(multi_variable_data %>% select(-c(NAME, GEOID)) %>% select(c(Public_transit_work_prop, No_home_workers_prop , Internet_subscription_prop ,  College_undergraduate_prop , Depart_work_6AM_629AM_prop , Travel_time_work_30_to_34_mins_prop , Attached_1_unit_building_prop, Household_income_60k_to_70k_prop, Lives_outside_principal_city_prop)))
-
-p1 = multi_variable_data %>% dplyr::filter(str_detect(NAME,"Maryland")) %>%
-  ggplot() + geom_histogram(aes(x = Household_income_60k_to_70k_prop))
+#Plotting histograms against each other to compare data of both states. x can equal any variable from the census
+p1 = multi_variable_data_combo %>% dplyr::filter(str_detect(NAME,"Maryland")) %>%
+  ggplot() + geom_histogram(aes(x = College_undergraduate_prop_combo))
   
-p2 = multi_variable_data %>% dplyr::filter(str_detect(NAME,"Pennsylvania")) %>%
-  ggplot() + geom_histogram(aes(x = Household_income_60k_to_70k_prop))
+p2 = multi_variable_data_combo %>% dplyr::filter(str_detect(NAME,"Pennsylvania")) %>%
+  ggplot() + geom_histogram(aes(x = College_undergraduate_prop_combo))
 
+#Displays histograms
 p1 / p2
 
-model = lm(data = multi_variable_data, formula = vehicle_ownership_prop ~ Depart_work_6AM_629AM_prop + Household_income_60k_to_70k_prop + Renter_housing_prop + Structure_built_1939_earlier_prop + One_worker_at_home_prop + College_undergraduate_prop + Travel_time_work_30_to_34_mins_prop + Public_transit_work_prop)
-
-summary(model)
-
-step(model)
-
-data_model = step(model)
-
-plot(model)
-multi_variable_data$predict = predict(model)
+#Creates the linear model
+model_combo = lm(data = multi_variable_data_combo, formula = vehicle_ownership_prop_combo ~ College_undergraduate_prop_combo + Travel_time_work_30_to_34_mins_prop_combo + Structure_built_1939_earlier_prop_combo + Vision_difficulty_prop_combo + White_race_prop_combo + Internet_subscription_prop_combo + Attached_1_unit_building_prop_combo)
 
 
-#all_county_data %>%
-#ggplot() +
-#geom_point(aes(x=B08016_002,y=B08203_001),colour="red") +
-#geom_line(data=,aes(x=B08016_002,y=predict),size=1,alpha=0.3,colour="blue") +
-#theme_bw()
-#plot(data=harford_dff,x=B08016_002,y=B08203_001)
-#abline(model)
+# Summarizing Data --------------------------------------------------------
 
+
+#Summary of the model
+summary(model_combo)
+
+#Uses backwards selection/elimination in step-wise function to finds fit of variables into model
+step(model_combo)
+
+#Residual plots of model
+plot(model_combo)
+
+#Plots the combined data as x-axis and vehicle ownership as y-axis (graph not used currently)
+#multi_variable_data_combo %>%
+  #ggplot() + theme_bw() +
+  #geom_point(aes(x=data_model_combo,y=vehicle_ownership_prop_combo)) + # Plots predicted vs actual
+  #geom_line(aes(x=data_model_combo,y=vehicle_ownership_prop_combo),color="blue",linetype=1) + 
+  # Plots actual data
+  #scale_x_continuous(limits=c(4,6)) +
+  #scale_y_continuous(limits=c(4,6)) +
+  #ggtitle("Model of combined states") + # Title of plot
+  #xlab("Variable Proportions") + # x axis title
+  #ylab("Proportion of Household Vehicle Ownership") # y axis title
